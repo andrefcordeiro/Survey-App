5db7b20328deb767de9e1a8a6f702100943d94e6
andrefcordeiro <andrefc36@gmail.com>
2023-07-13 16:40:25 -0300

    Created backend spring application

---
+++ backend/src/test/java/com/project/surveyapp/SurveyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.surveyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
