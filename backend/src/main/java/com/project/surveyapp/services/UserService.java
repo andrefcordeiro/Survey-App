91d519a2b1236acd62689646540f30baf67d081a
andrefcordeiro <andrefc36@gmail.com>
2023-07-16 20:18:48 -0300

    Fixing error that caused Coordinators and Respondents to not be inserted in the database

--- backend/src/main/java/com/project/surveyapp/services/UserService.java
+++ backend/src/main/java/com/project/surveyapp/services/UserService.java
@@ -1,8 +1,16 @@
 package com.project.surveyapp.services;
 
+import com.project.surveyapp.entities.Coordinator;
+import com.project.surveyapp.entities.RespondedSurvey;
+import com.project.surveyapp.entities.Respondent;
 import com.project.surveyapp.entities.User;
+import com.project.surveyapp.entities.enums.UserRole;
+import com.project.surveyapp.repositories.CoordinatorRepository;
+import com.project.surveyapp.repositories.RespondentRepository;
 import com.project.surveyapp.repositories.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,12 +22,28 @@ public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private CoordinatorRepository coordinatorRepository;
+
+    @Autowired
+    private RespondentRepository respondentRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
     }
 
-    public User createUser(User user) {
-        return userRepository.save(user);
+    public void createUser(User user) {
+
+        if (user.getRole().equals(UserRole.COORDINATOR)){
+            Coordinator c = new Coordinator();
+            BeanUtils.copyProperties(user, c);
+            coordinatorRepository.save(c);
+
+        } else {
+            Respondent r = new Respondent();
+            BeanUtils.copyProperties(user, r);
+            respondentRepository.save(r);
+        }
     }
 }
